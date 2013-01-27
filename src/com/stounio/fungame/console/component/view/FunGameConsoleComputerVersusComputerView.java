6eb68412093fb81a1e2fcd0378b5278de6e75c9d
Guillaume Pichot <guillaume.pichot@gmail.com>
2013-01-27 16:06:32 +0000

    Replace BufferedReader with Scanner

--- src/com/stounio/fungame/console/component/view/FunGameConsoleComputerVersusComputerView.java
+++ src/com/stounio/fungame/console/component/view/FunGameConsoleComputerVersusComputerView.java
@@ -7,8 +7,7 @@ import static com.stounio.fungame.resource.FunGameConstants.HELP_COMMAND;
 import static com.stounio.fungame.resource.FunGameConstants.RESTART_COMMAND;
 import static com.stounio.fungame.resource.FunGameConstants.SECTION_SEPARATOR;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import com.stounio.fungame.console.component.FunGameConsoleComponent;
 import com.stounio.fungame.console.component.FunGameConsoleComponentParameter;
@@ -18,47 +17,43 @@ import com.stounio.fungame.console.component.action.FunGameConsoleExitAction;
 import com.stounio.fungame.console.component.action.FunGameConsoleRestartAction;
 import com.stounio.fungame.resource.service.ResourceService;
 
-public class FunGameConsoleComputerVersusComputerView implements
-		FunGameConsoleComponent {
+public class FunGameConsoleComputerVersusComputerView implements FunGameConsoleComponent {
 
-	@Override
-	public FunGameConsoleComponentResult execute(
-			FunGameConsoleComponentParameter parameter) {
-		internalDisplay();
-		displayDefaultCommands(parameter);
-		return readPlayerInput();
-	}
+    @Override
+    public FunGameConsoleComponentResult execute(FunGameConsoleComponentParameter parameter) {
+        internalDisplay();
+        displayDefaultCommands(parameter);
+        return readPlayerInput();
+    }
 
-	private void internalDisplay() {
-		System.out.println(SECTION_SEPARATOR);
-		System.out.println(ResourceService.getService().getResource(FUNGAME_COMPUTER_VERSUS_COMPUTER_MODE_TITLE_KEY));
-		System.out.println(SECTION_SEPARATOR);
-	}
+    private void internalDisplay() {
+        System.out.println(SECTION_SEPARATOR);
+        System.out.println(ResourceService.getService().getResource(FUNGAME_COMPUTER_VERSUS_COMPUTER_MODE_TITLE_KEY));
+        System.out.println(SECTION_SEPARATOR);
+    }
 
-	private void displayDefaultCommands(
-			FunGameConsoleComponentParameter parameter) {
-		FunGameConsoleDefaultCommandsView funGameConsoleDefaultCommandsView = new FunGameConsoleDefaultCommandsView();
-		funGameConsoleDefaultCommandsView.execute(parameter);
-	}
-	
-	private FunGameConsoleComponentResult readPlayerInput() {
-		try{
-			System.out.println(ResourceService.getService().getResource(FUNGAME_MENU_CHOICE_KEY));
-			InputStreamReader converter = new InputStreamReader(System.in);
-			BufferedReader in = new BufferedReader(converter);
-			String input = in.readLine();
-			if(HELP_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleComputerVersusComputerHelpView(), null);
-			}
-			if(EXIT_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleExitAction(), null);
-			}
-			if(RESTART_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleRestartAction(), null);
-			}
-			return new FunGameConsoleComponentResult(new FunGameConsoleComputerPlayAction(), null);
-		}catch(Exception e){
-			return new FunGameConsoleComponentResult(new FunGameConsoleComputerVersusComputerView(), null);
-		}
-	}
+    private void displayDefaultCommands(FunGameConsoleComponentParameter parameter) {
+        FunGameConsoleDefaultCommandsView funGameConsoleDefaultCommandsView = new FunGameConsoleDefaultCommandsView();
+        funGameConsoleDefaultCommandsView.execute(parameter);
+    }
+
+    private FunGameConsoleComponentResult readPlayerInput() {
+        try {
+            System.out.println(ResourceService.getService().getResource(FUNGAME_MENU_CHOICE_KEY));
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (HELP_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleComputerVersusComputerHelpView(), null);
+            }
+            if (EXIT_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleExitAction(), null);
+            }
+            if (RESTART_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleRestartAction(), null);
+            }
+            return new FunGameConsoleComponentResult(new FunGameConsoleComputerPlayAction(), null);
+        } catch (Exception e) {
+            return new FunGameConsoleComponentResult(new FunGameConsoleComputerVersusComputerView(), null);
+        }
+    }
 }
--- src/com/stounio/fungame/console/component/view/FunGameConsoleHumanVersusComputerView.java
+++ src/com/stounio/fungame/console/component/view/FunGameConsoleHumanVersusComputerView.java
@@ -7,9 +7,8 @@ import static com.stounio.fungame.resource.FunGameConstants.HELP_COMMAND;
 import static com.stounio.fungame.resource.FunGameConstants.RESTART_COMMAND;
 import static com.stounio.fungame.resource.FunGameConstants.SECTION_SEPARATOR;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 import com.stounio.fungame.console.component.FunGameConsoleComponent;
 import com.stounio.fungame.console.component.FunGameConsoleComponentParameter;
@@ -24,50 +23,48 @@ import com.stounio.fungame.resource.service.ResourceService;
 
 public class FunGameConsoleHumanVersusComputerView implements FunGameConsoleComponent {
 
-	@Override
-	public FunGameConsoleComponentResult execute(
-			FunGameConsoleComponentParameter parameter) {
-		internalDisplay();
-		displayDefaultCommands(parameter);
-		FunGameConsoleComponentResult result = readPlayerInput();
-		return result;
-	}
-	
-	private void internalDisplay() {
-		System.out.println(SECTION_SEPARATOR);
-		System.out.println(ResourceService.getService().getResource(FUNGAME_HUMAN_VERSUS_COMPUTER_MODE_TITLE_KEY));
-		System.out.println(SECTION_SEPARATOR);
-		GamePlayService gamePlayService = GamePlayServiceFactory.getFactory().getInstance();
-		List<GamePlayOption> gamePlayOptions = gamePlayService.getGamePlayOptions();
-		for(GamePlayOption gamePlayOption : gamePlayOptions){
-			System.out.println("- '" + gamePlayOption.getIdentifier() + "'");
-		}
-	}
+    @Override
+    public FunGameConsoleComponentResult execute(FunGameConsoleComponentParameter parameter) {
+        internalDisplay();
+        displayDefaultCommands(parameter);
+        FunGameConsoleComponentResult result = readPlayerInput();
+        return result;
+    }
 
-	private void displayDefaultCommands(
-			FunGameConsoleComponentParameter parameter) {
-		FunGameConsoleDefaultCommandsView funGameConsoleDefaultCommandsView = new FunGameConsoleDefaultCommandsView();
-		funGameConsoleDefaultCommandsView.execute(parameter);
-	}
-	
-	private FunGameConsoleComponentResult readPlayerInput() {
-		try{
-			System.out.println(ResourceService.getService().getResource(FUNGAME_MENU_CHOICE_KEY));
-			InputStreamReader converter = new InputStreamReader(System.in);
-			BufferedReader in = new BufferedReader(converter);
-			String input = in.readLine();
-			if(HELP_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleHumanVersusComputerHelpView(), null);
-			}
-			if(EXIT_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleExitAction(), null);
-			}
-			if(RESTART_COMMAND.equals(input)){
-				return new FunGameConsoleComponentResult(new FunGameConsoleRestartAction(), null);
-			}
-			return new FunGameConsoleComponentResult(new FunGameConsoleHumanPlayAction(), new FunGameConsoleComponentParameter(input));
-		}catch(Exception e){
-			return new FunGameConsoleComponentResult(new FunGameConsoleHumanVersusComputerView(), null);
-		}
-	}
+    private void internalDisplay() {
+        System.out.println(SECTION_SEPARATOR);
+        System.out.println(ResourceService.getService().getResource(FUNGAME_HUMAN_VERSUS_COMPUTER_MODE_TITLE_KEY));
+        System.out.println(SECTION_SEPARATOR);
+        GamePlayService gamePlayService = GamePlayServiceFactory.getFactory().getInstance();
+        List<GamePlayOption> gamePlayOptions = gamePlayService.getGamePlayOptions();
+        for (GamePlayOption gamePlayOption : gamePlayOptions) {
+            System.out.println("- '" + gamePlayOption.getIdentifier() + "'");
+        }
+    }
+
+    private void displayDefaultCommands(FunGameConsoleComponentParameter parameter) {
+        FunGameConsoleDefaultCommandsView funGameConsoleDefaultCommandsView = new FunGameConsoleDefaultCommandsView();
+        funGameConsoleDefaultCommandsView.execute(parameter);
+    }
+
+    private FunGameConsoleComponentResult readPlayerInput() {
+        try {
+            System.out.println(ResourceService.getService().getResource(FUNGAME_MENU_CHOICE_KEY));
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (HELP_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleHumanVersusComputerHelpView(), null);
+            }
+            if (EXIT_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleExitAction(), null);
+            }
+            if (RESTART_COMMAND.equals(input)) {
+                return new FunGameConsoleComponentResult(new FunGameConsoleRestartAction(), null);
+            }
+            return new FunGameConsoleComponentResult(new FunGameConsoleHumanPlayAction(),
+                    new FunGameConsoleComponentParameter(input));
+        } catch (Exception e) {
+            return new FunGameConsoleComponentResult(new FunGameConsoleHumanVersusComputerView(), null);
+        }
+    }
 }
